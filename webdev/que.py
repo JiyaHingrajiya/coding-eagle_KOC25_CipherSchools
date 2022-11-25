12a5c411f7c9005f8ba8cee5ce2366311d49a1b8
JiyaHingrajiya <115679754+JiyaHingrajiya@users.noreply.github.com>
2022-11-25 18:05:15 +0530

    Delete webdev directory

--- webdev/que.py
+++
@@ -1,9 +0,0 @@
-b=70
-if 500<b<1000:
-    print("buy a dress")
-elif 1100<b<2000:
-    print("buy a pair of shoes")
-elif 2500<b<=3000:
-    print("get both a dress and pair of shoes")
-else:
-    print("cant get this amount of money ")
